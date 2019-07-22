72d8600fc2f772ae98eb5e869028ddf4d7d91fed
GW_19 <gw_19@GW-19s-iMac.local>
2019-07-22 17:03:12 +0900

    First Commit

---
+++ Ayo~Bro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ayo~Bro
+//
+//  Created by GW_19 on 22/07/2019.
+//  Copyright © 2019 GW_19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
