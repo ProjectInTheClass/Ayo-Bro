08b73dddff69ba7725dbce5e601b86642ab14f48
전정준 <cpot5620@naver.com>
2019-08-07 13:21:14 +0900

    카카오 맵 중심 좌표 이동

--- KakaoApi/KakaoMapViewController.swift
+++ KakaoApi/KakaoMapViewController.swift
@@ -8,18 +8,34 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, MTMapViewDelegate {
+class ViewController: UIViewController, MTMapViewDelegate, CLLocationManagerDelegate {
     var mapView: MTMapView?
+    var mapMarker: MTMapPOIItem?
+    var mapPoint: MTMapPoint?
+    var locationManager : CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView = MTMapView(frame: self.view.bounds)
+        mapMarker = MTMapPOIItem()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        //let coor = locationManager.location?.coordinate
+        
+        mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.619666, longitude: 127.059685))
+        mapView?.setMapCenter(mapPoint!, animated: true)
         
         if let mapView = mapView {
             mapView.delegate = self
             mapView.baseMapType = .standard
             self.view.addSubview(mapView)
         }
+        
+        
+        
     }
 }
-
