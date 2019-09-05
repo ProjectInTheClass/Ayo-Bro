5cf31fdee5f4db01e73181ed2629bc2f8d48b5a1
JinGoo Kim <wlsrn3684@naver.com>
2019-09-05 13:00:38 +0900

    세부 정보

---
+++ Ayo~Bro/Home/SearchCustomTabBar/DetailInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailInfoTableViewCell.swift
+//  Ayo~Bro
+//
+//  Created by A09 on 05/09/2019.
+//  Copyright © 2019 GW_19. All rights reserved.
+//
+
+import UIKit
+
+class DetailInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
